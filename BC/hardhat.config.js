772272130342d4de0af92b6eb270e91617e30618
Sourav Suman <souravsuman@Souravs-MacBook-Air.local>
2023-12-08 10:30:01 +0530

    added network

--- BC/hardhat.config.js
+++ BC/hardhat.config.js
@@ -1,5 +1,6 @@
 // require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,27 +8,76 @@ module.exports = {
     compilers: [
       {
         version: "0.8.17",
-        settings:{
+        settings: {
           optimizer: {
             enabled: true,
             runs: 200,
           },
-        }
+        },
       },
       {
         version: "0.8.20",
-        settings:{
+        settings: {
           optimizer: {
             enabled: true,
             runs: 200,
           },
-        }
-      }
+        },
+      },
     ],
   },
   networks: {
-    local: {
-      url: "http://localhost:24012/rpc"
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      chainId: 534351,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
     },
-}
+    base: {
+      url: "https://base-goerli.public.blastapi.io",
+      chainId: 84531,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    arbitrumGoerli: {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      chainId: 421613,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    celo: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      chainId: 44787,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    Mantle: {
+      url: "https://rpc.testnet.mantle.xyz",
+      chainId: 5001,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    Okx: {
+      url: "https://testrpc.x1.tech/",
+      chainId: 195,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    polygonzkevm: {
+      url: "https://rpc.public.zkevm-test.net",
+      chainId: 1442,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    local: {
+      url: "http://localhost:24012/rpc",
+    },
+  },
 };
